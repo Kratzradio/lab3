6f87c03c5a1f95de0e24b2287a8a804e3a555156
Maria Kersten <mkersten@pc08.pool.tp1.uni-duesseldorf.de>
2015-11-04 18:45:54 +0100

    sandwich

--- cmplx_func.cxx
+++ cmplx_func.cxx
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <complex>
 
+using namespace std;
+
+complex<double> func(double y) {
+      
+      complex<double> j = complex<double>(0,1.0);
+      complex<double> z = tan(y*(1.0+j));
+      
+      return z;
+}
+
 int main(){
 
   // Example how to declare a single complex variable:
-  complex<double> c;
-
+   complex<double> x;
+  int N = 100;
+  complex<double> p[N];
+  double dphi = 6.0/(N-1);		//Schrittabstände
+  double phi = -3;
+  
+  for(int k = 0; k <= N; k++){
+	  //x = complex<double> (0,phi);
+	  //p[k] = exp(x);
+	 p[k] = func(phi);
+	 phi = (k*dphi)-3;   
+  }
+  
+  for(int k = 0; k <=N; k++)
+	  cout << (k*dphi)-3 << "\t" << real(p[k]) << "\t" << imag(p[k]) << "\t" << abs(p[k]) << endl;
+  
   // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
+  //complex<double> d = complex<double>(1.0, 2.0); 
 
   return 0;
 }
--- newton.cxx
+++ newton.cxx
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <complex>
 
+using namespace std;
+
+complex<double> func(complex<double> x){
+  
+  x = x-((x*x*x - 1.0)/(3.0*x*x));
+  
+  
+ return x;
+}
+
 int main(){
-
-  // Example how to declare a single complex variable:
-  complex<double> c;
-
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
-
+  
+  complex<double> z0;
+  complex<double> z;
+  complex<double> dz;
+  int i;
+  
+ 
+  for(double k =-2; k<=2; k+=0.5){
+    for(double j=-2; j<=2; j+=0.5){
+      
+	z0 = complex<double>(k, j);
+	  i = 0;
+	  dz = 1;
+	  while(i<=400 && abs(dz)>=1e-8){
+            z = func(z0);
+	    dz = z-z0;
+	    z0 = z;
+	    i++;
+	  }
+	cout << k << "\t" << j << "\t" << i << endl;
+    }
+  }
   return 0;
 }
